de0c9404790dd4be0cda5982a83057fc68ff620f
jason <jasoncheng9@gmail.com>
2020-10-13 00:54:27 -0700

    added 5 second timeout and close for after 300 word test completes

--- typingtest_selenium.py
+++ typingtest_selenium.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
+import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
+count = 0
 
 
 driver.get("https://typing-speed-test.aoeu.eu/?lang=en")
@@ -9,6 +11,11 @@ current = driver.find_element_by_id('currentword')
 bar = driver.find_element_by_id('input')
 
 
-while True:
+while True and count < 300:
     word = driver.find_element_by_class_name("currentword").text
     bar.send_keys(word + ' ')
+    count += 1
+
+time.sleep(5)
+
+driver.quit()
