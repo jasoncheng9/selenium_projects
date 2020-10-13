c7a7da5b9e20f2425ff391ee8f296e4df637a44b
jason <jasoncheng9@gmail.com>
2020-10-12 23:49:46 -0700

    First commit of typingtest_selnium.

---
+++ typingtest_selnium.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+#from selenium.webdriver.common.keys import keys
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+
+driver.get("https://typing-speed-test.aoeu.eu/?lang=en")
+current = driver.find_element_by_id('currentword')
+bar = driver.find_element_by_id('input')
+
+
+while True:
+    word = driver.find_element_by_class_name("currentword").text
+    bar.send_keys(word + ' ')
