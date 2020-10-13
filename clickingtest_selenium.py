7f0e7b4e207b93f55a2374011d7264e7775a194e
jason <jasoncheng9@gmail.com>
2020-10-13 14:10:02 -0700

    adding clicking test to portfolio

---
+++ clickingtest_selenium.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+
+driver.get("https://clickspeedtest.com/")
+current = driver.find_element_by_id('clicker')
+
+
+
+while True:
+    python_button = driver.find_elements_by_xpath("/html/body/div[1]/div[1]/div[5]/div/button[1]")[0]
+    python_button.click()
