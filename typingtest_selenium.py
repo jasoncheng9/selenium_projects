7123895f7da1cbf750048eeadefba35f616ca5a2
jason <jasoncheng9@gmail.com>
2020-10-14 21:35:49 -0700

    updated typingtest_selenium to be in a function.

--- typingtest_selenium.py
+++ typingtest_selenium.py
@@ -1,21 +1,27 @@
 from selenium import webdriver
 import time
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-count = 0
+
+def main():
+
+    PATH = "C:\Program Files (x86)\chromedriver.exe"
+    driver = webdriver.Chrome(PATH)
+    count = 0
+
+    driver.get("https://typing-speed-test.aoeu.eu/?lang=en")
+    current = driver.find_element_by_id('currentword')
+    bar = driver.find_element_by_id('input')
 
 
-driver.get("https://typing-speed-test.aoeu.eu/?lang=en")
-current = driver.find_element_by_id('currentword')
-bar = driver.find_element_by_id('input')
+    while True and count < 300:
+        word = driver.find_element_by_class_name("currentword").text
+        bar.send_keys(word + ' ')
+        count += 1
+
+    time.sleep(5)
+
+    driver.quit()
 
 
-while True and count < 300:
-    word = driver.find_element_by_class_name("currentword").text
-    bar.send_keys(word + ' ')
-    count += 1
-
-time.sleep(5)
-
-driver.quit()
+if __name__ == '__main__':
+    main()
